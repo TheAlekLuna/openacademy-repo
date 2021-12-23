ac7bcc62e477acc4098ca821728e7f4e7dcaf9dd
Alejandro Luna <aluna@vauxoo.com>
2021-12-23 14:41:29 -0700

    [ADD] Second module 'Session' added, restoration point created

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -6,20 +6,14 @@ class Course(models.Model):
     _name = 'openacademy.course'
     _description = 'Ver OpenAcademyCourse'
 
-    name = fields.Char(string='Title', required = True)
+    name = fields.Char(string='Title', required=True)
     description = fields.Text()
 
+class Session(models.Model):
+    _name = 'openacademy.session'
+    _description = 'Ver OpenAcademySession'
 
-# class openacademy(models.Model):
-#     _name = 'openacademy.openacademy'
-#     _description = 'openacademy.openacademy'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
